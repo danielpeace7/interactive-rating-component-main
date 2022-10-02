824799b01bd5c49f808f709662fdebf8601c7f3b
danielpeace7 <danielpeace7@gmail.com>
2022-10-02 20:59:13 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function submit() {
+  var rating = document.getElementById("one").value;
+  alert("The number is " +rating)
+}
+
+// var rating = document.getElementsByClassName("number").onclick = function () {
+//   if (rating === document.getElementById("one")) {
+//     document.getElementsByClassName("select").innerHTML = "You selected 1 out of 5"
+//   }
+// }
