293dbfb363a799ea69bec7f0b0b2408457247985
danielpeace7 <danielpeace7@gmail.com>
2023-01-18 15:01:35 +0100

    second commit

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-function submit() {
-  var rating = document.getElementById("one").value;
-  alert("The number is " +rating)
+const getNumber = document.getElementsByClassName('number');
+const select = document.getElementById('rating');
+const submit = document.getElementById('submit');
+const hidden = document.querySelector('.hidden');
+const card = document.querySelector('.main-card')
+
+for (let i = 0; i < getNumber.length; i++) {
+  getNumber[i].addEventListener('click', getRating);  
 }
 
-// var rating = document.getElementsByClassName("number").onclick = function () {
-//   if (rating === document.getElementById("one")) {
-//     document.getElementsByClassName("select").innerHTML = "You selected 1 out of 5"
-//   }
-// }
+var getValue;
+
+function getRating(e) {  
+  getValue = e.target.value
+  // console.log(getValue);
+  rating.innerHTML = getValue;
+}
+
+submit.addEventListener('click', displayRating);
+
+function displayRating() {
+  if(hidden.classList.contains("hidden")) {
+    hidden.classList.remove("hidden");
+    card.style.display = "none";
+  } else {
+    hidden.classList.add("hidden");
+    card.style.display = "block";
+  }
+}
